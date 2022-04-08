dc8c8cd7c4f7feb2e7e477fcd71ebad96100fcd3
elzomor13 <52608235+elzomor13@users.noreply.github.com>
2022-04-09 01:13:42 +0200

    Method reference and passing functions Examples added

--- src/com/company/firstClassFunctions/IV_MethodReferences.java
+++ src/com/company/firstClassFunctions/IV_MethodReferences.java
@@ -16,18 +16,22 @@ public class IV_MethodReferences {
     public static void main(String[] args) {
         // Referencing to static method
         Consumer<String> print = System.out::println;
+        // Using lambdas ==> = str -> System.out.println(str);
         print.accept("Hello");
 
         // Referencing to create instance
         Supplier<Cat> print1 = Cat::new;
-        Cat c = print1.get();
+        // Using lambdas ==> = () -> new Cat();
+        final Cat c = print1.get();
 
         // Reference to get method from class
         VoidNoArgFunction print2 = c::makeSound;
+        // Using lambdas ==> = () -> c.makeSound();
         print2.apply();
 
         // Reference to java objects
         BiFunction<Integer, Integer, Integer> getSum = Integer::sum;
+        // Using lambdas ==> = (x, y) -> Integer.sum(x + y);
         System.out.println(getSum.apply(2, 3));
 
     }
